4e56a0bc96aee4fa1e002952abbb4e17f32dab4c
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-10-02 19:23:35 +0530

    Add: Added To Get Number

--- AddressBook.java
+++ AddressBook.java
@@ -14,16 +14,18 @@ public class AddressBook {
 		addressBook.showMenu();
 	}
 
-	// Menu Method to show the different operation which can be performed in address book.
+	// Menu Method to show the different operation which can be performed in address
+	// book.
 	public void showMenu() {
 		try {
 			while (true) {
-				System.out.println("" + "1. Create New Address Book \n" + "2. Continue with existing Address Book \n"
+				System.out.println("\n" + "1. Create New Address Book \n" + "2. Continue with existing Address Book \n"
 						+ "3. Show All Address Books \n" + "4. Search person by location in all Address Books\n"
-						+ "5. Exit");
+						+ "5. Search Phone Number by Name \n" + "6. EXIT");
 				int choice = scanner.nextInt();
 
 				switch (choice) {
+				// Creates A New Address Book Here.
 				case 1:
 					System.out.println("\n: Enter a name for Address Book :");
 					String newBook = scanner.next();
@@ -35,6 +37,7 @@ public class AddressBook {
 						createContact(contactList, contactBook, newBook);
 					break;
 
+				// Address Book Choice.
 				case 2:
 					System.out.println(contactBook.keySet());
 					System.out.println("Which address book do you want to access?");
@@ -47,6 +50,7 @@ public class AddressBook {
 						System.out.println("\n: Book Not Found :");
 					break;
 
+				// Display All Address Books List.
 				case 3:
 					int serialNo = 1;
 					for (String book : contactBook.keySet()) {
@@ -56,12 +60,21 @@ public class AddressBook {
 					System.out.println("\n" + contactBook);
 					break;
 
+				// Searching The Person By Location.
 				case 4:
 					System.out.println("\n: Enter Name of City or State :");
 					String nameForLocation = scanner.next();
 					searchByLocation(nameForLocation);
 					break;
 
+				// Searching Phone Number By Name.
+				case 5:
+					System.out.println("\n: Enter First Name :");
+					String nameForContact = scanner.next();
+					getContactNumber(nameForContact);
+					break;
+
+				// Terminates The Menu.
 				default:
 					System.exit(0);
 				}
@@ -71,31 +84,41 @@ public class AddressBook {
 		}
 	}
 
-	// createContact Method
+	// createContact(AddressBook) Method
 	private void createContact(LinkedList<Contacts> contactList, HashMap<String, LinkedList<Contacts>> contactBook,
 			String addressBook) {
 		try {
 			boolean run = true;
 			while (run) {
-				System.out.println("" + "1. Add a New Contact Detail \n" + "2. Show All Contacts \n"
+				System.out.println("\n" + "1. Add a New Contact Detail \n" + "2. Show All Contacts \n"
 						+ "3. Update a Contact \n" + "4. Delete a Contact \n" + "5. Exit");
 				int choice = scanner.nextInt();
 
 				switch (choice) {
+
+				// Adds A New Contact In Address Book.
 				case 1: {
 					LinkedList<Contacts> multiContactInBook = addContact(contactList);
 					contactBook.put(addressBook, multiContactInBook);
 				}
 					break;
+
+				// Displays The Contact In Existing Address Book
 				case 2:
 					displayContact(contactList);
 					break;
+
+				// Updates A Contact With Persons First Name.
 				case 3:
 					updateContact(contactList);
 					break;
+
+				// Delete A Contact With Persons First Name.
 				case 4:
 					deleteContact(contactList);
 					break;
+
+				// Terminates The Menu
 				default:
 					run = false;
 				}
@@ -181,42 +204,49 @@ public class AddressBook {
 					contactInfo.setFirstName(newFirstName);
 				}
 					break;
+					
 				case 2: {
 					System.out.println("Enter new Last Name");
 					String newLastName = scanner.next();
 					contactInfo.setLastName(newLastName);
 				}
 					break;
+					
 				case 3: {
 					System.out.println("Enter new Phone Number");
 					String newPhone = scanner.next();
 					contactInfo.setPhoneNumber(newPhone);
 				}
 					break;
+					
 				case 4: {
 					System.out.println("Enter new Email");
 					String newEmail = scanner.next();
 					contactInfo.setEmail(newEmail);
 				}
 					break;
+					
 				case 5: {
 					System.out.println("Enter new City");
 					String newCity = scanner.next();
 					contactInfo.setCity(newCity);
 				}
 					break;
+					
 				case 6: {
 					System.out.println("Enter new State");
 					String newState = scanner.next();
 					contactInfo.setState(newState);
 				}
 					break;
+					
 				case 7: {
 					System.out.println("Enter new Zip code");
 					String newZipcode = scanner.next();
 					contactInfo.setZipCode(newZipcode);
 				}
 					break;
+					
 				}
 				System.out.println("\n: Updated Successfully :");
 			}
@@ -247,25 +277,45 @@ public class AddressBook {
 	public Hashtable<String, List<String>> searchByLocation(String nameForLocation) {
 		try {
 			Hashtable<String, List<String>> searchResult = new Hashtable<>();
-			List<String> contactList=new ArrayList<>();;
+			List<String> contactList = new ArrayList<>();
+			;
 			for (String keyOfBook : contactBook.keySet()) {
 				for (int index = 0; index < contactBook.get(keyOfBook).size(); index++) {
 
 					if (contactBook.get(keyOfBook).get(index).getCity().equals(nameForLocation))
 						contactList.add(contactBook.get(keyOfBook).get(index).getFirstName());
-					
+
 					if (contactBook.get(keyOfBook).get(index).getState().equals(nameForLocation))
 						contactList.add(contactBook.get(keyOfBook).get(index).getFirstName());
 				}
 				if (!contactList.isEmpty())
 					searchResult.put(keyOfBook, contactList);
 			}
-			System.out.println("Persons In City or State ->"+searchResult);
+			System.out.println("Persons In City or State ->" + searchResult);
 			return searchResult;
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
+	}
+
+	// getContactNumber Method (search phone number by name)
+	private void getContactNumber(String nameOfContact) {
+		try {
+			for (String keyOfBook : contactBook.keySet()) {
+				for (int index = 0; index < contactBook.get(keyOfBook).size(); index++) {
+					if (contactBook.get(keyOfBook).get(index).getFirstName().equals(nameOfContact)) {
+
+						String lastName = contactBook.get(keyOfBook).get(index).getLastName();
+						String phone = contactBook.get(keyOfBook).get(index).getPhoneNumber();
+
+						System.out.println(keyOfBook + " : " + nameOfContact + " " + lastName + " --> " + phone + "\n");
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	// searchName Method
